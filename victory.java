f3127421a23f1972f5d142a805c027ebfd8c3c3a
Anders <51828522+Lion4567714@users.noreply.github.com>
2020-09-10 19:05:00 -0700

    Added Goal Sheet 01 Items

---
+++ victory.java
@@ -0,0 +1,14 @@
+public class victory {
+    public static void main(String[] args) {
+        int i;
+        System.out.println("//////////////////////");
+        for(i = 0; i < 5; i++) {
+            isMine();
+        }
+    }
+
+    public static void isMine() {
+        System.out.println("|| Victory is mine! ||");
+        System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+    }
+}
