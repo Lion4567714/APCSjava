5cbfeffe3d0c5513089c5852ccc05d56b63ab905
Anders <gillilanda@rsd407.org>
2021-05-11 08:27:19 -0700

    Completed GS14-02 IsReverse

--- goalSheet14/src/isReverseClass.java
+++ goalSheet14/src/isReverseClass.java
@@ -8,8 +8,8 @@ import java.util.Scanner;
  * string, is considered to be its own reverse.
  *
  * @author Anders Gilliland
- * @version 0.1
- * @since 2021-5-10
+ * @version 1.0
+ * @since 2021-5-11
  */
 
 public class isReverseClass {
@@ -17,10 +17,22 @@ public class isReverseClass {
         Scanner console = new Scanner(System.in);
 
         System.out.println("Input two strings to be compared:");
-        //System.out.println(isReverse(console.next(), console.next(), 0));
+        System.out.println(isReverse(console.next(), console.next(), 0));
     }
 
-    //public static boolean isReverse (String s1, String s2, int index) {
+    public static boolean isReverse (String s1, String s2, int index) {
+        if (s1.length() != s2.length()) {
+            return false;
+        } else if (s1.length() == 0) {
+            return true;
+        }
 
-    //}
+        if (index == s1.length()) {
+            return true;
+        } else {
+            return (Character.toLowerCase(s1.charAt(index)) ==
+                    Character.toLowerCase(s2.charAt(s2.length() - index - 1)) &&
+                    isReverse(s1, s2, index + 1));
+        }
+    }
 }
